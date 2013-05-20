f4016ef8a21d2967267d1659fb57b540064b1b54
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-05-20 00:41:54 -0700

    two space

--- weblistener.js
+++ weblistener.js
@@ -10,15 +10,15 @@ zen.register_commands('weblistener.js', [])
 app.use(express.bodyParser())
 
 app.post('/:app', function(req, res) {
-    console.log(req.params.app)
-    var message = {
-        app: req.params.app,
-        body: req.body
-    };
-    var sending = JSON.stringify(message)
-    console.log(sending)
-    zen.redis.publish('web_in', sending)
-    res.send('', 200)
+  console.log(req.params.app)
+  var message = {
+    app: req.params.app,
+    body: req.body
+  };
+  var sending = JSON.stringify(message)
+  console.log(sending)
+  zen.redis.publish('web_in', sending)
+  res.send('', 200)
 })
 
 app.listen(weblistener_config.port)
